5229af35b04b26ed900cf17d33bd699701576758
Prashant Agrawal <prashantagrawal2107@gmail.com>
2024-08-09 02:02:06 +0530

    complete user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,43 +1,251 @@
-import {useSelector} from 'react-redux'
-import { TextInput , Button } from 'flowbite-react'
+import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+// import {
+//   updateStart,
+//   updateSuccess,
+//   updateFailure,
+//   deleteUserStart,
+//   deleteUserSuccess,
+//   deleteUserFailure,
+//   signoutSuccess,
+// } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-  const {currentUser} = useSelector(state => state.user)
+  const { currentUser, error, loading } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({});
+  const filePickerRef = useRef();
+  const dispatch = useDispatch();
+  
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    
+    setImageFileUploading(true);
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          'Could not upload image (File must be less than 2MB)'
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+        setImageFileUploading(false);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+          setFormData({ ...formData, profilePicture: downloadURL });
+          setImageFileUploading(false);
+        });
+      }
+    );
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setUpdateUserError(null);
+  //   setUpdateUserSuccess(null);
+  //   if (Object.keys(formData).length === 0) {
+  //     setUpdateUserError('No changes made');
+  //     return;
+  //   }
+  //   if (imageFileUploading) {
+  //     setUpdateUserError('Please wait for image to upload');
+  //     return;
+  //   }
+  //   try {
+  //     dispatch(updateStart());
+  //     const res = await fetch(`/api/user/update/${currentUser._id}`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(formData),
+  //     });
+  //     const data = await res.json();
+  //     if (!res.ok) {
+  //       dispatch(updateFailure(data.message));
+  //       setUpdateUserError(data.message);
+  //     } else {
+  //       dispatch(updateSuccess(data));
+  //       setUpdateUserSuccess("User's profile updated successfully");
+  //     }
+  //   } catch (error) {
+  //     dispatch(updateFailure(error.message));
+  //     setUpdateUserError(error.message);
+  //   }
+  // };
+  // const handleDeleteUser = async () => {
+  //   setShowModal(false);
+  //   try {
+  //     dispatch(deleteUserStart());
+  //     const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+  //       method: 'DELETE',
+  //     });
+  //     const data = await res.json();
+  //     if (!res.ok) {
+  //       dispatch(deleteUserFailure(data.message));
+  //     } else {
+  //       dispatch(deleteUserSuccess(data));
+  //     }
+  //   } catch (error) {
+  //     dispatch(deleteUserFailure(error.message));
+  //   }
+  // };
+
+  // const handleSignout = async () => {
+  //   try {
+  //     const res = await fetch('/api/user/signout', {
+  //       method: 'POST',
+  //     });
+  //     const data = await res.json();
+  //     if (!res.ok) {
+  //       console.log(data.message);
+  //     } else {
+  //       dispatch(signoutSuccess());
+  //     }
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-          <img src={currentUser.profilePicture} alt='user'
-            className='rounded-full w-full h-full border-8 border-[light-gray]' />
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={imageFileUploadProgress<100 ? `${imageFileUploadProgress}%` : ''}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imageFileUploadProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
+          <img
+            src={imageFileUrl || currentUser.profilePicture}
+            alt='user'
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              'opacity-60'
+            }`}
+          />
         </div>
+        {imageFileUploadError && (
+          <Alert color='failure'>{imageFileUploadError}</Alert>
+        )}
         <TextInput
           type='text'
           id='username'
           placeholder='username'
           defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <TextInput
           type='email'
           id='email'
           placeholder='email'
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
         <TextInput
           type='password'
           id='password'
           placeholder='password'
+          onChange={handleChange}
         />
         <Button
           type='submit'
           gradientDuoTone='purpleToBlue'
           outline
-        >Update</Button>
+          disabled={loading || imageFileUploading}
+        >
+          {loading ? 'Loading...' : 'Update'}
+        </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
-      <div className='text-red-500 flex justify-between mt-5'>
-          <span>Delete Account</span>
-          <span>Sign Out</span>
-      </div>
+      
     </div>
-  )
+  );
 }
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -19,7 +19,7 @@ export default function DashSidebar() {
             <Sidebar.Items>
                 <Sidebar.ItemGroup className='flex flex-col gap-1'>
                    <Link to='/dashboard?tab=profile'>
-                        <Sidebar.Item active={tab==='profile'} icon={HiUser} label="User" labelColor='dark'>
+                        <Sidebar.Item as='div' active={tab==='profile'} icon={HiUser} label="User" labelColor='dark'>
                              Profile
                         </Sidebar.Item>
                    </Link>
